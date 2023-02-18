6183cd90ff4408f53fa987fa1fca99719ddfcae7
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2023-02-18 20:23:21 +0630

    Role, permission models and data design

--- database/migrations/2020_01_01_000004_create_users_table.php
+++ database/migrations/2020_01_01_000004_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->boolean('owner')->default(false);
             $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
