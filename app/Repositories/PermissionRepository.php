e2b768e9efe46091c1b46eec1aa950a3d6bfceac
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2023-02-19 07:28:01 +0630

    Permission repository

---
+++ app/Repositories/PermissionRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\RolePermission;
+use App\Models\RoleUser;
+use App\Models\Staff;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class PermissionRepository
+{
+    public function getByUser($user_id)
+    {
+        
+    }
+
+    public function show($id)
+    {
+        $item = Permission::find($id);
+
+        return $item;
+    }
+
+    public function store($data)
+    {
+        $item = new Permission();
+        $item->name = $data->name;
+        $item->label = Str::slug($data->name);
+        $item->flag = 0;
+        $item->save();
+
+        foreach ($data->assign_roles as $role) {
+            RolePermission::create([
+                'role_id' => $role,
+                'permission_id' => $item->id
+            ]);
+        }
+
+        return $item;
+    }
+
+    public function update($id, $data)
+    {
+        $item = Permission::findOrFail($id);
+        $item->name = $data->name;
+        $item->label = Str::slug($data->name);
+        $item->flag = 0;
+        $item->save();
+
+        return $item;
+    }
+
+    public function destroy($id)
+    {
+        $item = Permission::findOrFail($id);
+        $item->delete();
+
+        return true;
+    }
+}
